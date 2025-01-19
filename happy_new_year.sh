99f0acaf46d605919fb839884a96e57f1b4dffe7
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-20 00:25:33 +0300

    Add configuration interface

--- happy_new_year.sh
+++ happy_new_year.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Configuration Interface
+
+COPYRIGHT_PATTERN="Copyright (\c\) 1999-****"
+MASK_SYMBOL="*"
+
+CURRENT_YEAR=$(date +"%Y") # set manually if your system has invalid date
+
+TARGETS=(
+	"."
+)
+
+
 print_success_message() {
 cat << "EOF"
 .      *    *           *.       *   .                      *     .
