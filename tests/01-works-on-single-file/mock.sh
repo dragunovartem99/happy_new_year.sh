e1df1bf57734c9e000c4a58ee01aa807c2c660f4
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-20 05:04:43 +0300

    Update tests/01

--- tests/01-works-on-single-file/mock.sh
+++ tests/01-works-on-single-file/mock.sh
@@ -3,8 +3,8 @@
 TODAY="2025"
 REGEX="[0-9]{4}"
 
-OLD_COPYRIGHT="Copyright \(c\) 2022-$REGEX"
-NEW_COPYRIGHT="Copyright \(c\) 2022-$TODAY"
+OLD_COPYRIGHT="Copyright \(2022-$REGEX\)"
+NEW_COPYRIGHT="Copyright \(2022-$TODAY\)"
 
 TARGETS=(
 	"tests/01-works-on-single-file/code.js"
