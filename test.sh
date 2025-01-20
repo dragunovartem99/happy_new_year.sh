25b744063facae5444dce14415c058911f0e443b
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-20 03:29:13 +0300

    Simplify syntax

--- test.sh
+++ test.sh
@@ -2,19 +2,20 @@
 
 source generate_script.sh
 
-for dir in ./playground/*/; do
+for dir in playground/*/; do
 	generate_script "${dir}configuration.test.sh" | bash
 
-	actual_diff=$(git -C "$dir" diff)
+	actual_diff=$(git diff $dir)
 	expected_diff=$(cat "${dir}git.diff")
 
 	if [ "$actual_diff" == "$expected_diff" ]; then
 		echo "Git diff matches the expected output in $dir"
+		git checkout $dir
 	else
 		echo "Git diff does not match the expected output in $dir"
 		echo "Expected Diff:"
-		echo "$expected_diff"
+		echo $expected_diff
 		echo "Actual Diff:"
-		echo "$actual_diff"
+		echo $actual_diff
 	fi
 done
