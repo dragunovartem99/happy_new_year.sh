0a3c7349a056a126372f2c215485c382b60ca430
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-20 05:33:19 +0300

    Remove a mistake from mock

--- tests/02-works-with-specified-owner/mock.sh
+++ tests/02-works-with-specified-owner/mock.sh
@@ -1,10 +1,10 @@
 # Configuration
 
-TODAY="2020"
+TODAY="2019"
 REGEX="[0-9]{4}"
 
-OLD_COPYRIGHT="Copyright \(c\) 2003-$REGEX OpenAI, Inc.Copyright"
-NEW_COPYRIGHT="Copyright \(c\) 2003-$TODAY OpenAI, Inc.Copyright"
+OLD_COPYRIGHT="Copyright \(c\) 2003-$REGEX OpenAI, Inc."
+NEW_COPYRIGHT="Copyright \(c\) 2003-$TODAY OpenAI, Inc."
 
 TARGETS=(
 	"tests/02-works-with-specified-owner/code.py"
